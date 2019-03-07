4677fdbffae3738fd9a2fed36378731893b17cb2
kieriosity <philip.maynard@threatpoint.io>
2019-03-07 07:28:07 -0500

    Removed the sample implementation.

--- xing_number_validator.py
+++ xing_number_validator.py
@@ -61,18 +61,3 @@ def crossing_number_validation(xn):
 
     else:
         return False
-
-
-while True:
-    xn = input('Enter the crossing number: ')
-    if xn == 'exit' or xn == 'quit' or xn == 'q':
-        quit(print('Have a safe day!'))
-
-    value = crossing_number_validation(xn)
-
-    if value is True:
-        print("This crossing number is VALID.")
-        print("Please enter another crossing number or type \'q\' to quit. \n")
-    else:
-        print("This crossing number is INVALID.")
-        print("Please enter another crossing number or type \'q\' to quit. \n")
